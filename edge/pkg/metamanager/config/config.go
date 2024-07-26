6b6fdd212c75d78ef306daf886aa8f8e9a41849e
llmmqq8023 <273337875@qq.com>
2024-07-26 15:24:14 +0800

    print events on edge

--- edge/pkg/metamanager/config/config.go
+++ edge/pkg/metamanager/config/config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"sync"
 
-	edgedconfig "github.com/kubeedge/kubeedge/edge/pkg/edged/config"
 	metaserverconfig "github.com/kubeedge/kubeedge/edge/pkg/metamanager/metaserver/config"
 	"github.com/kubeedge/kubeedge/pkg/apis/componentconfig/edgecore/v1alpha2"
 )
@@ -13,14 +12,12 @@ var once sync.Once
 
 type Configure struct {
 	v1alpha2.MetaManager
-	NodeId string
 }
 
 func InitConfigure(m *v1alpha2.MetaManager) {
 	once.Do(func() {
 		Config = Configure{
 			MetaManager: *m,
-			NodeId:      edgedconfig.Config.HostnameOverride,
 		}
 		metaserverconfig.InitConfigure(Config.MetaManager.MetaServer)
 	})
--- edge/pkg/metamanager/dao/meta.go
+++ edge/pkg/metamanager/dao/meta.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/kubeedge/kubeedge/common/constants"
 	"github.com/kubeedge/kubeedge/edge/pkg/common/dbm"
-	metaManagerConfig "github.com/kubeedge/kubeedge/edge/pkg/metamanager/config"
 )
 
 // constant metatable name reference
@@ -22,17 +21,13 @@ const (
 
 // Meta metadata object
 type Meta struct {
-	DistributedKey string `orm:"column(distributed_key); size(325); pk"`
-	Key            string `orm:"column(key); size(256)"`
-	Type           string `orm:"column(type); size(32)"`
-	Node           string `orm:"column(node_name); size(64)"`
-	Value          string `orm:"column(value); null; type(text)"`
+	Key   string `orm:"column(key); size(256); pk"`
+	Type  string `orm:"column(type); size(32)"`
+	Value string `orm:"column(value); null; type(text)"`
 }
 
 // SaveMeta save meta to db
 func SaveMeta(meta *Meta) error {
-	meta.DistributedKey = metaManagerConfig.Config.NodeId + "/" + meta.Key
-	meta.Node = metaManagerConfig.Config.NodeId
 	num, err := dbm.DBAccess.Insert(meta)
 	klog.V(4).Infof("Insert affected Num: %d, %v", num, err)
 	if err == nil || IsNonUniqueNameError(err) {
@@ -53,14 +48,14 @@ func IsNonUniqueNameError(err error) bool {
 
 // DeleteMetaByKey delete meta by key
 func DeleteMetaByKey(key string) error {
-	num, err := dbm.DBAccess.QueryTable(MetaTableName).Filter("key", key).Filter("node_name", metaManagerConfig.Config.NodeId).Delete()
+	num, err := dbm.DBAccess.QueryTable(MetaTableName).Filter("key", key).Delete()
 	klog.V(4).Infof("Delete affected Num: %d, %v", num, err)
 	return err
 }
 
 // DeleteMetaByKeyAndPodUID delete meta by key and podUID
 func DeleteMetaByKeyAndPodUID(key, podUID string) (int64, error) {
-	sqlStr := fmt.Sprintf("DELETE FROM meta WHERE key = '%s' and node_name = '%s' and value LIKE '%%%s%%'", key, metaManagerConfig.Config.NodeId, podUID)
+	sqlStr := fmt.Sprintf("DELETE FROM meta WHERE key = '%s' and value LIKE '%%%s%%'", key, podUID)
 	res, err := dbm.DBAccess.Raw(sqlStr).Exec()
 	if err != nil {
 		klog.Errorf("delete pod by key %s and podUID %s failed, err: %v", key, podUID, err)
@@ -69,21 +64,8 @@ func DeleteMetaByKeyAndPodUID(key, podUID string) (int64, error) {
 	return res.RowsAffected()
 }
 
-// DeleteMetaByKeyAndNodeId delete meta by key and node id
-func DeleteMetaByKeyAndNodeId(key, nodeId string) (int64, error) {
-	sqlStr := fmt.Sprintf("DELETE FROM meta WHERE key = '%s' and node_name = '%s'", key, nodeId)
-	res, err := dbm.DBAccess.Raw(sqlStr).Exec()
-	if err != nil {
-		klog.Errorf("delete pod by key %s and nodeId %s failed, err: %v", key, nodeId, err)
-		return 0, err
-	}
-	return res.RowsAffected()
-}
-
 // UpdateMeta update meta
 func UpdateMeta(meta *Meta) error {
-	meta.DistributedKey = metaManagerConfig.Config.NodeId + "/" + meta.Key
-	meta.Node = metaManagerConfig.Config.NodeId
 	num, err := dbm.DBAccess.Update(meta) // will update all field
 	klog.V(4).Infof("Update affected Num: %d, %v", num, err)
 	return err
@@ -91,33 +73,21 @@ func UpdateMeta(meta *Meta) error {
 
 // InsertOrUpdate insert or update meta
 func InsertOrUpdate(meta *Meta) error {
-	distributedKey := metaManagerConfig.Config.NodeId + "/" + meta.Key
-	klog.V(3).Infof("DistributedKey: %s", distributedKey)
-	_, err := dbm.DBAccess.Raw("INSERT OR REPLACE INTO meta (distributed_key, key, type, node_name, value) VALUES (?,?,?,?,?)", distributedKey, meta.Key, meta.Type, metaManagerConfig.Config.NodeId, meta.Value).Exec() // will update all field
-	klog.V(3).Infof("Update error %v of resource: %#v", err, meta)
+	_, err := dbm.DBAccess.Raw("INSERT OR REPLACE INTO meta (key, type, value) VALUES (?,?,?)", meta.Key, meta.Type, meta.Value).Exec() // will update all field
+	klog.V(4).Infof("Update result %v", err)
 	return err
 }
 
 // UpdateMetaField update special field
 func UpdateMetaField(key string, col string, value interface{}) error {
-	updatemap := map[string]interface{}{col: value}
-	// If the pk is updated
-	if col == "key" {
-		updatemap["distributed_key"] = fmt.Sprintf("%s/%v", metaManagerConfig.Config.NodeId, value)
-		klog.Infof("In UpdateMetaField: distributed_key changed on node %s from %v to %v", metaManagerConfig.Config.NodeId, key, value)
-	}
-	num, err := dbm.DBAccess.QueryTable(MetaTableName).Filter("key", key).Filter("node_name", metaManagerConfig.Config.NodeId).Update(updatemap)
+	num, err := dbm.DBAccess.QueryTable(MetaTableName).Filter("key", key).Update(map[string]interface{}{col: value})
 	klog.V(4).Infof("Update affected Num: %d, %v", num, err)
 	return err
 }
 
 // UpdateMetaFields update special fields
 func UpdateMetaFields(key string, cols map[string]interface{}) error {
-	if newkey, ok := cols["key"]; ok {
-		cols["distributed_key"] = fmt.Sprintf("%s/%v", metaManagerConfig.Config.NodeId, newkey)
-		klog.Infof("In UpdateMetaFields: distributed_key changed on node %s from %v to %v", metaManagerConfig.Config.NodeId, key, newkey)
-	}
-	num, err := dbm.DBAccess.QueryTable(MetaTableName).Filter("key", key).Filter("node_name", metaManagerConfig.Config.NodeId).Update(cols)
+	num, err := dbm.DBAccess.QueryTable(MetaTableName).Filter("key", key).Update(cols)
 	klog.V(4).Infof("Update affected Num: %d, %v", num, err)
 	return err
 }
@@ -125,7 +95,7 @@ func UpdateMetaFields(key string, cols map[string]interface{}) error {
 // QueryMeta return only meta's value, if no error, Meta not null
 func QueryMeta(key string, condition string) (*[]string, error) {
 	meta := new([]Meta)
-	_, err := dbm.DBAccess.QueryTable(MetaTableName).Filter(key, condition).Filter("node_name", metaManagerConfig.Config.NodeId).All(meta)
+	_, err := dbm.DBAccess.QueryTable(MetaTableName).Filter(key, condition).All(meta)
 	if err != nil {
 		return nil, err
 	}
@@ -140,7 +110,7 @@ func QueryMeta(key string, condition string) (*[]string, error) {
 // QueryAllMeta return all meta, if no error, Meta not null
 func QueryAllMeta(key string, condition string) (*[]Meta, error) {
 	meta := new([]Meta)
-	_, err := dbm.DBAccess.QueryTable(MetaTableName).Filter(key, condition).Filter("node_name", metaManagerConfig.Config.NodeId).All(meta)
+	_, err := dbm.DBAccess.QueryTable(MetaTableName).Filter(key, condition).All(meta)
 	if err != nil {
 		return nil, err
 	}
@@ -148,35 +118,9 @@ func QueryAllMeta(key string, condition string) (*[]Meta, error) {
 	return meta, nil
 }
 
-// QueryAllMetas return all metas from distributed database, if no error, Meta not null
-func QueryAllMetas(key string, condition string) (*[]Meta, error) {
-	metas := new([]Meta)
-	_, err := dbm.DBAccess.QueryTable(MetaTableName).Filter(key, condition).All(metas)
-	if err != nil {
-		return nil, err
-	}
-
-	return metas, nil
-}
-
-// QueryMetasByNodeId return some one node's metas from distributed database, if no error, Meta not null
-func QueryMetasByNodeId(key, condition, nodeId string) (*[]Meta, error) {
-	metas := new([]Meta)
-	var err error
-	if len(nodeId) != 0 {
-		_, err = dbm.DBAccess.QueryTable(MetaTableName).Filter(key, condition).Filter("node_name", nodeId).All(metas)
-	} else {
-		_, err = dbm.DBAccess.QueryTable(MetaTableName).Filter(key, condition).All(metas)
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	return metas, nil
-}
-
 // SaveMQTTMeta saves mqtt container data in sqlites
 // When egdecore starts, edged will start mqtt container
+// FIXME: cleanup this code when the static pod mqtt broker no longer needs to be compatible
 func SaveMQTTMeta(nodeName string) error {
 	flag := true
 	mqttData := coreV1.Pod{
@@ -185,7 +129,7 @@ func SaveMQTTMeta(nodeName string) error {
 			APIVersion: "v1",
 		},
 		ObjectMeta: metaV1.ObjectMeta{
-			Name:      constants.DeafultMosquittoContainerName,
+			Name:      constants.DefaultMosquittoContainerName,
 			Namespace: "default",
 			UID:       uuid.NewUUID(),
 		},
@@ -231,7 +175,7 @@ func SaveMQTTMeta(nodeName string) error {
 	}
 	mqttDataStr, _ := json.Marshal(mqttData)
 	mqttMeta := Meta{
-		Key:   fmt.Sprintf("default/pod/%s", constants.DeafultMosquittoContainerName),
+		Key:   fmt.Sprintf("default/pod/%s", constants.DefaultMosquittoContainerName),
 		Type:  "pod",
 		Value: string(mqttDataStr),
 	}
