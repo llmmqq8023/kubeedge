ef06ff197540d989c2be1b4ad13ae01bf0f965af
llmmqq8023 <273337875@qq.com>
2024-08-13 18:50:15 +0800

    cloucore_base
    Signed-off-by: llmmqq8023 <273337875@qq.com>

---
+++ cloud_base/pkg/edgecontroller/types/nvidia.go
@@ -0,0 +1,9 @@
+package types
+
+// NvidiaGPUStatus Stores health status of a single GPU
+type NvidiaGPUStatus struct {
+	// Store the physical GPU id
+	ID string `json:"id" protobuf:"bytes,1,opt,name=id"`
+	// Health status
+	Healthy bool `json:"healthy" protobuf:"varint,2,opt,name=healthy"`
+}
